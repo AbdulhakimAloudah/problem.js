931016c99e9fd2056f6e5491923202f93c8dd158
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-11 16:48:42 +0300

    sf

--- problem.js
+++ problem.js
@@ -1,5 +1,17 @@
-function isPalindrome(x) {
-    let reverseX = x.toLowerCase().split('').reverse();
-    if(x.toLowerCase()==reverseX.join('')){
-      return true;
-    } else {
+
+
+//1
+// function isPalindrome(x) {
+//     let reverseX = x.toLowerCase().split('').reverse();
+//     if(x.toLowerCase()==reverseX.join('')){
+//       return true;
+//     } else {
+
+
+//2
+// const highestRank = (arr) =>
+//   arr.sort(
+//     (a, b) =>
+//       arr.filter((val) => val === b).length -
+//         arr.filter((val) => val === a).length || b - a
+//   )[0];
